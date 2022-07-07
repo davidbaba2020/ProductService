70cade1971859560c3cfa0c720a8ada8e30d6c0f
David Baba <davidbabasalifu2020.com>
2022-07-07 02:53:41 +0100

    Completed my product CQRS service

--- src/main/java/com/davacom/ProductService/query/api/controller/ProductQueryController.java
+++ src/main/java/com/davacom/ProductService/query/api/controller/ProductQueryController.java
@@ -2,6 +2,8 @@ package com.davacom.ProductService.query.api.controller;
 
 
 import com.davacom.ProductService.core.api.model.ProductRestModel;
+import com.davacom.ProductService.query.api.queries.GetProductQuery;
+import org.axonframework.messaging.responsetypes.ResponseTypes;
 import org.axonframework.queryhandling.QueryGateway;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,6 +24,11 @@ public class ProductQueryController {
 
     @GetMapping
     public List<ProductRestModel> getAllProducts() {
-        return null;
+        GetProductQuery getProductQuery = new GetProductQuery();
+
+        List<ProductRestModel> productRestModels = queryGateway.query(getProductQuery,
+                ResponseTypes.multipleInstancesOf(ProductRestModel.class)).join();
+
+        return productRestModels;
     }
 }
